51ffbc7d45fdb4ad0dbda885ae120322e87de9f3
caraclarke <cara.clarke8@gmail.com>
2016-03-29 13:47:01 -0400

    App.jsx updated, info added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,33 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+  
+  constructor(props) {
+    suoer(props);
+    this.state = { // initial state
+      username: 'caraclarke',
+      userData: [],
+      userRepos: [],
+      perPage: 5
+    }
+  }
+  // this.state main component
+  // this.props nested components
   render () {
     return (
-      <div>Hey</div>
+      {this.state.username}
     )
   }
+}
+
+App.propTypes = {
+  clientId: React.PropTypes.string,
+  clientSecret: React.PropTypes.string
+};
+
+App.defaultProps = {
+  clientId: '70bf6e347b41d53fe9ac',
+  clientSecret: '84da34924989a93d7601ceacd254a61417298b5a'
 }
 
 export default App;
